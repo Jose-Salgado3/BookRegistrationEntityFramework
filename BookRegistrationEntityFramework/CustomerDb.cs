1cf4c9f06f56ab17ee8b446828bff8a87d64f701
Jose Salgado <Josalgado03@gmail.com>
2019-08-07 22:49:31 -0700

    Added database class for customers

---
+++ BookRegistrationEntityFramework/CustomerDb.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRegistrationEntityFramework
+{
+    static class CustomerDb
+    {
+        /// <summary>
+        /// Returns all Customers from the data base
+        /// </summary>
+        /// <returns></returns>
+        public static List<Customer> GetCustomers()
+        {
+            //Create instance of DB context
+            var db = new BookRegistrationEntities();
+
+            //Use DB context to retrieve all Customers
+            //Use LINQ to query database(cast to list)
+            //Query syntax
+            //List<Customer> customers =
+            //    (from c in db.Customer
+            //    select c).ToList();
+
+            //LINQ Method Syntax -Same query as above
+            List<Customer> customers =
+                db.Customer
+                .ToList();
+
+            return customers;
+        }
+
+        /// <summary>
+        /// Adds a customer. Returns the newly added customer with the Customer Id populated
+        /// </summary>
+        /// <param name="c">The new Customer to be added</param>
+        /// <returns></returns>
+        public static Customer AddCustomer(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
