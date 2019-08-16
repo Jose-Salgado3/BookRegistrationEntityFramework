19b729045b6e79f804a02c82676eae5fa546fa24
Jose Salgado <Josalgado03@gmail.com>
2019-08-16 07:35:23 -0700

    Finished CustomerDB class

--- BookRegistrationEntityFramework/CustomerDb.cs
+++ BookRegistrationEntityFramework/CustomerDb.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,6 +47,31 @@ namespace BookRegistrationEntityFramework
                 context.SaveChanges();
                 //Return newly added customer with CustomerId(Identity column) populated
                 return c;
+            }
+        }
+        public static Customer UpdateCustomer(Customer c)
+        {
+            // If a customer ID = 0 EF will know this is a new customer.
+            using (var context = new BookRegistrationEntities())
+            {
+                //Adds to context, not DB(technically) so it starts tracking it
+                context.Customer.Add(c);
+                //Tell EF we are updating an existing entity
+                context.Entry(c).State = EntityState.Modified;
+                context.SaveChanges();
+                return c;
+            }
+        }
+
+        public static void DeleteCustomer(Customer c)
+        {
+            using (var context = new BookRegistrationEntities)
+            {
+                //begin tracking
+                context.Customer.Add(c);
+
+                context.Entry(c).State = EntityState.Deleted;
+                int rowsAffrected = context.SaveChanges();
             }
         }
     }
